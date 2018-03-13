c1ad983d61e69d0641861d267c821dbc9a2b4c9c
aaskhura <aaskhura@AASKHURA-IN.in.oracle.com>
2018-03-13 11:50:55 +0530

    Add_File_1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+console.log("express created");
 app.use(express.static(path.join(__dirname, 'web')));
 console.log("getting index.html);
 
@@ -9,5 +10,5 @@ app.get('/', function(req, res){
     res.render('index.html');
 });
 
-app.listen(process.env.PORT || 3007);
+app.listen(process.env.PORT || 3000);
 console.log('Server running on port 3000');
